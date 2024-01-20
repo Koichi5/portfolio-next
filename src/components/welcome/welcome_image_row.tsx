0e2f4f94c3bab1662085b124f85c18e5ea13b7d8
Koichi5 <koichi20021217@gmail.com>
2024-01-20 22:48:26 +0900

    edit welcome images

--- src/components/welcome/welcome_image_row.tsx
+++ src/components/welcome/welcome_image_row.tsx
@@ -8,17 +8,17 @@ const images = [
     rotation: -3,
   },
   {
-    src: "https://lh3.googleusercontent.com/pw/ABLVV86dnc9x9TKp5eeI-rMZYHVYITgZUSDRPsn7VejZw-MSZMW2xAhpeDorAs6UX3MwCmomX4IpZM6Th9s7v3qFTCsU3EtS3rA7mOJYmMl01LKCRI4FPGxxTL_7Gh9GwXQVFI9e-f2-YnZIl4T2ABqiXibP=w1080-h1080-s-no-gm?authuser=0",
+    src: "https://lh3.googleusercontent.com/pw/ABLVV860bGEKHq9R_yOuWGS7tIFBXOzC0U4wCBVdG9-XmPI6GigqWc8pkil1cN1wq82ZIZ98yUkDx_R9X5weVSiRg-FrOMG6Ih9JsZCDfA9HEj6H7B1z5b_MBAb7OnTqkfHe33-hLrAPbCjX3X4DUzySvCl8=w400-h400-s-no-gm?authuser=0",
     title: "Second Image",
     rotation: 1,
   },
   {
-    src: "https://lh3.googleusercontent.com/pw/ABLVV85van4vIv3jH4lKIyouJ33mt0vgO6OiVbwo7LwWLzHQs3AdklmHo3DNGxhsXZ6r5uq1-ZeGjZDgmFFWFtid3wgSa2H9Bg--sXi1T1peoo-KuD3McFMKNP0NMBpWtiCoR4iNk6yXL-AxiFwT7zdZWr71=w320-h320-s-no-gm?authuser=0",
+    src: "https://lh3.googleusercontent.com/pw/ABLVV86mTH7kXqAu-2JD0cgRdVXBDpmM5zooPWienpJM7-lP1P1C_WqlBIdget5DS77way_zaNsEmUq4dBTiNpZXYUyJ9LM8T5Vqz-Zt4P037UJ1SAXHwvji-O8RR7aZmx-1rp232eozHYaP3vDLoOUrlPwP=w500-h500-s-no-gm?authuser=0",
     title: "Third Image",
     rotation: -1,
   },
   {
-    src: "https://lh3.googleusercontent.com/pw/ABLVV86mTH7kXqAu-2JD0cgRdVXBDpmM5zooPWienpJM7-lP1P1C_WqlBIdget5DS77way_zaNsEmUq4dBTiNpZXYUyJ9LM8T5Vqz-Zt4P037UJ1SAXHwvji-O8RR7aZmx-1rp232eozHYaP3vDLoOUrlPwP=w500-h500-s-no-gm?authuser=0",
+    src: "https://lh3.googleusercontent.com/pw/ABLVV86dnc9x9TKp5eeI-rMZYHVYITgZUSDRPsn7VejZw-MSZMW2xAhpeDorAs6UX3MwCmomX4IpZM6Th9s7v3qFTCsU3EtS3rA7mOJYmMl01LKCRI4FPGxxTL_7Gh9GwXQVFI9e-f2-YnZIl4T2ABqiXibP=w1080-h1080-s-no-gm?authuser=0",
     title: "Fourth Image",
     rotation: 2,
   },
