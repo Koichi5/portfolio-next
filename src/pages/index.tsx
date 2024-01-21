090f29480084045c02c2d8dcc00d972437d8f7c4
Koichi5 <koichi20021217@gmail.com>
2024-01-21 18:55:55 +0900

    delete tab provider and theme provider on index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { CustomThemeProvider } from "@/components/theme_context";
 import Footer from "@/pages/footer";
 import Header from "@/pages/header";
-import { TabProvider } from "@/components/tab_context";
 
 function Page() {
   return (
-    <CustomThemeProvider>
-      <TabProvider>
-        <Header />
-        {/* <Welcome />
-      <Articles />
-      <Suspense fallback={<Loading />}>
-        <About />
-        <Skills />
-        <Works />
-        <Contests />
-      </Suspense> */}
-        <Footer />
-      </TabProvider>
-    </CustomThemeProvider>
+    <div>
+      <Header />
+      <Footer />
+    </div>
   );
 }
 
