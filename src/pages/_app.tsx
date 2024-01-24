0d973ee6ddcc849c46bfc1fdd6efeeb357b9a4bc
Koichi5 <koichi20021217@gmail.com>
2024-01-25 03:01:05 +0900

    edit page title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import "./styles/globals.css";
 import type { AppProps } from "next/app";
 import { TabProvider } from "@/components/tab_context";
 import { CustomThemeProvider } from "@/components/theme_context";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CustomThemeProvider>
+      <Head>
+        <title>Portfolio</title>
+      </Head>
       <TabProvider>
         <Component {...pageProps} />
       </TabProvider>
