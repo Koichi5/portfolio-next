e82e278a53feea1cde51ad32b451a8fd30043631
Koichi5 <koichi20021217@gmail.com>
2024-01-26 14:53:33 +0900

    add metadata

---
+++ lib/metadata.tsx
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+import Favicon from "src/app/favicon.ico";
+
+export const metadata: Metadata = {
+  title: 'Portfolio',
+  icons: [{ rel: 'icon', url: Favicon.src }],
+};
